5468756afc2aa23fa5da06a4ed268dd16b21ff25
Sean Dolan <seandolan53@yahoo.com>
2021-11-16 11:50:46 +0000

    Changed the col size back to default

--- devmuscles/settings.py
+++ devmuscles/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'fsewrfawef'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['milestone-project-four-muscles.herokuapp.com', gi'localhost']
+ALLOWED_HOSTS = ['milestone-project-four-muscles.herokuapp.com', 'localhost']
 
 
 # Application definition
